51fbcfcc329d9b9aeb2e1bc25d336c7a2f7c736d
ahmedShoeb <sasan.talukder1@gmail.com>
2016-10-06 12:49:29 +1000

    Adding Manager portal page
    Contains the html page for the manager portal

---
+++ Project/managerHome.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!-->
+
+<html>
+	<head>
+		<?php require 'staffTitle.php';?>
+	<body>
+		<div class = "container">
+
+			<div class="clear"></div>
+			<section class= "main_section">		<br><br>
+				<div class= 'left_part'>
+					<a href = ""><input type="button" class = "manager_button" value="Assign Deliveries" /></a><br/>
+					<a href = ""><input type="button" class = "manager_button" value="New employee" /></a><br/>
+					<a href = ""><input type="button" class = "manager_button" value="View employees" /></a><br/>
+				</div>
+
+				<div class= 'right_part'>
+				 	<a href = "report.php"><input type="button" class = "manager_button" value="View report " /></a><br/>
+					<a href = ""><input type="button" class = "manager_button" value="Search order" /></a><br/>
+					<a href = ""><input type="button" class = "manager_button" value="Completed deliveries" /></a><br/>
+				</div>
+
+			</section>
+			<div class="clear"></div>
+			<footer class="page_footer">
+				&copy; Copywrite OnTheSpotPackages.com
+			</footer>
+       </div>
+	</body>
+</html>
